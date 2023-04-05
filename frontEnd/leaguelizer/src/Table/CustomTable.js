2bc9345b174a55db603cc65dc56b6087b46b67f3
SaliArnold <91786230+Arnold-21@users.noreply.github.com>
2023-04-05 09:18:37 +0300

    Finished 5

--- frontEnd/leaguelizer/src/Table/CustomTable.js
+++ frontEnd/leaguelizer/src/Table/CustomTable.js
@@ -183,7 +183,7 @@ export default function CustomTable(){
     const [ stadiumNameFilter, setStadiumNameFilter ] = React.useState("");
     
     var getUrlForStadiums = useCallback(() => {
-        var URL = "http://localhost:8000/stadiums/"
+        var URL = "http://35.174.171.199/stadiums/"
         if (stadiumNameFilter !== ""){
             URL += "?name=" + stadiumNameFilter;
         }
