61a2b1c816b6a43b1a6b644d594e21cc925f3c2d
SaliArnold <91786230+Arnold-21@users.noreply.github.com>
2023-05-01 17:02:19 +0300

    Custom User Model

--- lab/lab1_api/models.py
+++ lab/lab1_api/models.py
@@ -1,4 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create user models
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        REGULAR = "Regular", 'Regular'
+        MODERATOR = "Moderator", 'Moderator'
+        Admin = "Admin", 'Admin'
+
+    role = models.CharField(max_length=30, choices=Role.choices, default='R')
+    confirmation_code = models.CharField(max_length=20, blank=True)
+    confirmation_start = models.DateTimeField(blank=True, null=True)
+
+class UserDetail(models.Model):
+    genderChoices = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other')
+    ]
+    maritalChoices = [
+        ('M', 'Married'),
+        ('R', 'Relationship'),
+        ('S', 'Single')
+    ]
+
+    userName = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.CharField(max_length=200, blank=True)
+    location = models.CharField(max_length=100, blank=True)
+    birthday = models.DateField(blank=True)
+    gender = models.CharField(max_length=2, choices=genderChoices, blank=True)
+    marital = models.CharField(max_length=2, choices=maritalChoices, blank=True)
+
 
 # Create your models here.
 class Stadium(models.Model):
