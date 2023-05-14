b1149dc48f24317f43ab514f03abacfed0bf7e7d
SaliArnold <91786230+Arnold-21@users.noreply.github.com>
2023-05-14 18:43:07 +0300

    sql generation bugfix

--- lab/SQLQueries/userSqlGenerator.py
+++ lab/SQLQueries/userSqlGenerator.py
@@ -28,67 +28,67 @@ def insertDetail(i):
     birthday = fake.date()
     gender = choice(genders)
     marital = choice(maritals)
-    return f"(\'{i}\', \'{bio}\', \'{location}\', \'{birthday}\', \'{gender}\', \'{marital}\')"
+    return f"(\'{i}\', \'{bio}\', \'{location}\', \'{birthday}\', \'{gender}\', \'{marital}\', 12)"
 
-# insertText = "INSERT INTO lab1_api_user(\"email\", \"username\", \"password\", \"is_active\", \"role\", \"is_superuser\", \"first_name\", \"last_name\", \"is_staff\", \"date_joined\") values "
+insertText = "INSERT INTO lab1_api_user(\"email\", \"username\", \"password\", \"is_active\", \"role\", \"is_superuser\", \"first_name\", \"last_name\", \"is_staff\", \"date_joined\") values "
 
-# for i in range(10000 - 1):
-#     insertText += insertUser(i) + ", "
-# insertText += insertUser(9999) + ";\n"
+for i in range(10000 - 1):
+    insertText += insertUser(i) + ", "
+insertText += insertUser(9999) + ";\n"
 
-# with open("lab/SQLQueries/data6.sql", "a") as file:
-#         file.write(insertText)
+with open("lab/SQLQueries/data4.sql", "a") as file:
+        file.write(insertText)
 
-# insertText = "INSERT INTO lab1_api_userdetail(\"userName_id\", \"bio\", \"location\", \"birthday\", \"gender\", \"marital\") values "
+insertText = "INSERT INTO lab1_api_userdetail(\"userName_id\", \"bio\", \"location\", \"birthday\", \"gender\", \"marital\", \"paginationValue\") values "
 
-# for i in range(3,10002):
-#     insertText += insertDetail(i) + ", "
-# insertText += insertDetail(9999) + ";\n"
+for i in range(1,10001):
+    insertText += insertDetail(i) + ", "
+insertText += insertDetail(9999) + ";\n"
 
-# with open("lab/SQLQueries/data6.sql", "a") as file:
-#         file.write(insertText)
+with open("lab/SQLQueries/data4.sql", "a") as file:
+        file.write(insertText)
 
-NUM_TABLE = 1_000_000
-NUM_MANY = 10_000_000
-NUM_BATCH = 1_000
+# NUM_TABLE = 1_000_000
+# NUM_MANY = 10_000_000
+# NUM_BATCH = 1_000
 
-def updateStadium():
-    insertText = ""
+# def updateStadium():
+#     insertText = ""
 
-    for i in range(int(int(NUM_TABLE/100))):
-        insertText += f"update lab1_api_stadium set user_id = {i+3} where id >= {100*i} and id < {100*(i+1)};\n"
+#     for i in range(int(int(NUM_TABLE/100))):
+#         insertText += f"update lab1_api_stadium set user_id = {i+3} where id >= {100*i} and id < {100*(i+1)};\n"
 
-    with open("lab/SQLQueries/data6.sql", "a") as file:
-            file.write(insertText)
+#     with open("lab/SQLQueries/data6.sql", "a") as file:
+#             file.write(insertText)
 
-def updateClub():
-    insertText = ""
+# def updateClub():
+#     insertText = ""
 
-    for i in range(int(int(NUM_TABLE/100))):
-        insertText += f"update lab1_api_club set user_id = {i+3} where id >= {100*i} and id < {100*(i+1)};\n"
+#     for i in range(int(int(NUM_TABLE/100))):
+#         insertText += f"update lab1_api_club set user_id = {i+3} where id >= {100*i} and id < {100*(i+1)};\n"
 
-    with open("lab/SQLQueries/data6.sql", "a") as file:
-            file.write(insertText)
+#     with open("lab/SQLQueries/data6.sql", "a") as file:
+#             file.write(insertText)
 
-def updateCompetition():
-    insertText = ""
+# def updateCompetition():
+#     insertText = ""
 
-    for i in range(int(int(NUM_TABLE/100))):
-        insertText += f"update lab1_api_competition set user_id = {i+3} where id >= {100*i} and id < {100*(i+1)};\n"
+#     for i in range(int(int(NUM_TABLE/100))):
+#         insertText += f"update lab1_api_competition set user_id = {i+3} where id >= {100*i} and id < {100*(i+1)};\n"
 
-    with open("lab/SQLQueries/data6.sql", "a") as file:
-            file.write(insertText)
+#     with open("lab/SQLQueries/data6.sql", "a") as file:
+#             file.write(insertText)
 
-def updateMatches():
-    insertText = ""
+# def updateMatches():
+#     insertText = ""
 
-    for i in range(int(int(NUM_MANY/1000))):
-        insertText += f"update lab1_api_matchesplayed set user_id = {i+3} where id >= {100*i} and id < {100*(i+1)};\n"
+#     for i in range(int(int(NUM_MANY/1000))):
+#         insertText += f"update lab1_api_matchesplayed set user_id = {i+3} where id >= {100*i} and id < {100*(i+1)};\n"
 
-    with open("lab/SQLQueries/data6.sql", "a") as file:
-            file.write(insertText)
+#     with open("lab/SQLQueries/data6.sql", "a") as file:
+#             file.write(insertText)
 
-updateStadium()
-updateCompetition()
-updateClub()
-updateMatches()
+# updateStadium()
+# updateCompetition()
+# updateClub()
+# updateMatches()
